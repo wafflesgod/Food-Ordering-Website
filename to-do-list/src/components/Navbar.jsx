6e3436a3473d343b3ba3065d5088c9d373a2d12b
wafflesgod <huimingchang09@gmail.com>
2025-09-02 11:59:41 +0800

    style

--- to-do-list/src/components/Navbar.jsx
+++ to-do-list/src/components/Navbar.jsx
@@ -8,7 +8,6 @@ import { faRaspberryPi } from "@fortawesome/free-brands-svg-icons";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { faClipboard } from "@fortawesome/free-solid-svg-icons";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
@@ -20,19 +19,16 @@ const Navbar = () => {
       <nav className="navbar">
         <ul className="nav-links">
           <li>
-            <Link to="/home"><FontAwesomeIcon icon={faHouse} /> Home</Link>
+            <Link to="/home"><FontAwesomeIcon icon={faHouse} />Home</Link>
           </li>
           <li>
-            <Link to="/menu"><FontAwesomeIcon icon={faClipboard} /> Menu</Link>
+            <Link to="/menu"><FontAwesomeIcon icon={faClipboard} />Menu</Link>
           </li>
           <li>
-            <Link to="/about"><FontAwesomeIcon icon={faCircleInfo} /> About</Link>
+            <Link to="/about"><FontAwesomeIcon icon={faCircleInfo} />About</Link>
           </li>
           <li>
-            <Link to="/services"><FontAwesomeIcon icon={faBriefcase} /> Services</Link>
-          </li>
-          <li>
-            <Link to="/contact"><FontAwesomeIcon icon={faPhone} /> Contact</Link>
+            <Link to="/contact"><FontAwesomeIcon icon={faPhone} />Contact</Link>
           </li>
         </ul>
       </nav>
