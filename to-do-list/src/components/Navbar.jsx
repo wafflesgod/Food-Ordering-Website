c95dce8028598307ca4cc21299becb0a0516f934
wafflesgod <huimingchang09@gmail.com>
2025-09-02 11:19:03 +0800

    added Menu to Nav

--- to-do-list/src/components/Navbar.jsx
+++ to-do-list/src/components/Navbar.jsx
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Home from "./Home";
 import About from "./About"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRaspberryPi } from "@fortawesome/free-brands-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
         <div className="navbar-main">
             <div className="navbar-logo">
-                <Link to="/home">(logo) YumYum</Link>
+                <Link to="/home"><FontAwesomeIcon icon={faRaspberryPi} flip/>YumYum</Link>
             </div>
             <nav className="navbar">
                 <ul className="nav-links">
-                    <li><Link to="/home">Home</Link></li>
+                    <li className="faHouse"><Link to="/home"><FontAwesomeIcon icon={faHouse} /> Home</Link></li>
                     <li><Link to="/about">About</Link></li>
+                    <li><Link to="/Menu">Menu</Link></li>
                     <li><Link to="/services">Services</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
