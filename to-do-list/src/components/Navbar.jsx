1907011b5907a803ad8f6e008dbcc50a130ef09a
LightningIce <lightningiceivan@gmail.com>
2025-09-02 11:24:47 +0800

    create new file

--- to-do-list/src/components/Navbar.jsx
+++ to-do-list/src/components/Navbar.jsx
@@ -2,24 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Home from "./Home";
-import About from "./About"
+import About from "./About";
 
 const Navbar = () => {
-    return (
-        <div className="navbar-main">
-            <div className="navbar-logo">
-                <Link to="/home">(logo) YumYum</Link>
-            </div>
-            <nav className="navbar">
-                <ul className="nav-links">
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/services">Services</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="navbar-main">
+      <div className="navbar-logo">
+        <Link to="/home">(logo) YumYum</Link>
+      </div>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
