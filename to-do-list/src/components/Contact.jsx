478a2f6f17dc2ac7ff106a326740f795092266ad
LightningIce <lightningiceivan@gmail.com>
2025-09-02 11:32:42 +0800

    edited Contact.jsx

---
+++ to-do-list/src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import "./Contact.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Contact Us</h1>
+        <div>
+            Get in Touch
+        </div>
+        <div>
+            Send us a Message
+        </div>
+    </div>
+);
+}
